01d521b4891b7dcbed5cc9262b716c0909539dbe
Melcn <melaniearinosy@outlook.com>
2023-04-28 10:12:32 +0200

    create say-hello component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SayHelloComponent } from './say-hello/say-hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SayHelloComponent
   ],
   imports: [
     BrowserModule
